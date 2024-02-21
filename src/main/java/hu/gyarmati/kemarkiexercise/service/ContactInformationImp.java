b8877fb2209cef0affd493ce400d97db95f2c26f
gyarmatiadam <gyari12@gmail.com>
2024-02-21 18:43:33 +0100

    Write saveContactInformation method in ContactInformationImp

--- src/main/java/hu/gyarmati/kemarkiexercise/service/ContactInformationImp.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/service/ContactInformationImp.java
@@ -1,8 +1,11 @@
 package hu.gyarmati.kemarkiexercise.service;
 
+import hu.gyarmati.kemarkiexercise.domain.Address;
+import hu.gyarmati.kemarkiexercise.domain.ContactInformation;
 import hu.gyarmati.kemarkiexercise.dto.ContactInformationInfoDto;
 import hu.gyarmati.kemarkiexercise.dto.SaveAndUpdateContactInformationDto;
 import hu.gyarmati.kemarkiexercise.repository.ContactInformationRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ContactInformationImp implements ContactInformationService {
     private final ContactInformationRepository contactInformationRepository;
+    private final AddressService addressService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public ContactInformationImp(ContactInformationRepository contactInformationRepository) {
+    public ContactInformationImp(ContactInformationRepository contactInformationRepository, AddressService addressService, ModelMapper modelMapper) {
         this.contactInformationRepository = contactInformationRepository;
+        this.addressService = addressService;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public ContactInformationInfoDto saveAddress(SaveAndUpdateContactInformationDto saveAndUpdateContactInformationDto) {
-        return null;
+        Address address = addressService.findAddressById(saveAndUpdateContactInformationDto.getAddressId());
+        ContactInformation contactInformation = modelMapper.map(saveAndUpdateContactInformationDto, ContactInformation.class);
+        contactInformation.setAddress(address);
+        return modelMapper.map(contactInformationRepository.save(contactInformation), ContactInformationInfoDto.class);
     }
 }
