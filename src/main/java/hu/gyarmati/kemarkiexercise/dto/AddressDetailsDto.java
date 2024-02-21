5ade5d8714f747332a5f3cc19f49b25c889c7bdd
gyarmatiadam <gyari12@gmail.com>
2024-02-21 11:03:51 +0100

    Write PersonDetailsDto and add AddressDetailsDto

--- src/main/java/hu/gyarmati/kemarkiexercise/dto/AddressDetailsDto.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/dto/AddressDetailsDto.java
@@ -5,14 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PersonDetailsDto {
-    private Long id;
-    private String name;
-    private List<AddressDetailsDto> addressList;
+public class AddressDetailsDto {
 }
