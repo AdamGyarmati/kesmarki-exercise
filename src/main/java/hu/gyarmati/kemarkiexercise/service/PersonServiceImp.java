3761cc393d7494838eb67ef4e059793362f43766
gyarmatiadam <gyari12@gmail.com>
2024-02-21 20:24:34 +0100

    refactor deletePerson in PersonServiceImp,Address's Person object set null before delete Person

--- src/main/java/hu/gyarmati/kemarkiexercise/service/PersonServiceImp.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/service/PersonServiceImp.java
@@ -56,6 +56,7 @@ public class PersonServiceImp implements PersonService {
     @Override
     public void deletePerson(Long id) {
         Person person = findPersonById(id);
+        person.getAddressList().forEach(address -> address.setPerson(null));
         personRepository.delete(person);
     }
 
