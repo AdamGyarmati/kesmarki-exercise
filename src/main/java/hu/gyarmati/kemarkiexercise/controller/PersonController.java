80a8d5c255c590fee4871dc64912814131312ede
gyarmatiadam <gyari12@gmail.com>
2024-02-21 11:02:46 +0100

    Start write getPersonById method in PersonController and add PersonDetailsDto

--- src/main/java/hu/gyarmati/kemarkiexercise/controller/PersonController.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/controller/PersonController.java
@@ -1,5 +1,6 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
+import hu.gyarmati.kemarkiexercise.dto.PersonDetailsDto;
 import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.dto.SavePersonDto;
 import hu.gyarmati.kemarkiexercise.service.PersonService;
@@ -7,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -31,5 +29,10 @@ public class PersonController {
         log.info("Http request POST /api/persons with body: " + savePersonDto.toString());
         PersonInfoDto personInfoDto = personService.savePerson(savePersonDto);
         return new ResponseEntity<>(personInfoDto, CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDetailsDto> getPersonById(@PathVariable Long id) {
+        return null;
     }
 }
