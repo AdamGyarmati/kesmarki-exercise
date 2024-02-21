51f5aa0476cbdfa04b751a2c1aa33db408f63e72
gyarmatiadam <gyari12@gmail.com>
2024-02-21 11:12:17 +0100

    Write canGetPersonById test method and delete AddressDetailsDto AllArgsConstructor lombok annotation

--- src/test/java/hu/gyarmati/kemarkiexercise/controller/PersonControllerTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/controller/PersonControllerTest.java
@@ -1,6 +1,7 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.gyarmati.kemarkiexercise.dto.PersonDetailsDto;
 import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.service.PersonService;
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(PersonController.class)
@@ -39,5 +45,23 @@ public class PersonControllerTest {
         ).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(CREATED.value());
+    }
+
+    @DisplayName("Test for getPersonById method")
+    @Test
+    public void canGetPersonById() throws Exception {
+        PersonDetailsDto johnDoe = new PersonDetailsDto(1L, "John Doe", Collections.emptyList());
+
+        given(personService.getPersonById(1L))
+                .willReturn(johnDoe);
+
+        MockHttpServletResponse response = mvc.perform(
+                get("/api/persons/1").accept(APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(
+                objectMapper.writeValueAsString(johnDoe)
+        );
     }
 }
