45f34e7315e36cfb3d3add806748e3b9f88ecdc2
gyarmatiadam <gyari12@gmail.com>
2024-02-21 14:38:17 +0100

    Write canGetAddressById test method in AddressControllerTest

--- src/test/java/hu/gyarmati/kemarkiexercise/controller/AddressControllerTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/controller/AddressControllerTest.java
@@ -1,7 +1,9 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.gyarmati.kemarkiexercise.dto.AddressDetailsDto;
 import hu.gyarmati.kemarkiexercise.dto.AddressInfoDto;
+import hu.gyarmati.kemarkiexercise.dto.PersonDetailsDto;
 import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.service.AddressService;
 import org.junit.jupiter.api.DisplayName;
@@ -12,9 +14,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(AddressController.class)
@@ -37,5 +44,23 @@ public class AddressControllerTest {
         ).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(CREATED.value());
+    }
+
+    @DisplayName("Test for getAddressById method")
+    @Test
+    public void canGetAddressById() throws Exception {
+        AddressDetailsDto addressDetailsDto = new AddressDetailsDto(1L, "PERMANENT", Collections.emptyList());
+
+        given(addressService.getAddressById(1L))
+                .willReturn(addressDetailsDto);
+
+        MockHttpServletResponse response = mvc.perform(
+                get("/api/addresses/1").accept(APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(
+                objectMapper.writeValueAsString(addressDetailsDto)
+        );
     }
 }
