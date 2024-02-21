0a1d98ecd0289947fef71795415cbcbd0b1ce2a2
gyarmatiadam <gyari12@gmail.com>
2024-02-21 10:29:48 +0100

    Add CanSaveANewPerson method to PersonControllerTest

--- src/test/java/hu/gyarmati/kemarkiexercise/controller/PersonControllerTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/controller/PersonControllerTest.java
@@ -1,11 +1,22 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.service.PersonService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(PersonController.class)
 public class PersonControllerTest {
@@ -19,5 +30,14 @@ public class PersonControllerTest {
     @MockBean
     private PersonService personService;
 
+    @DisplayName("Test for savePerson method")
+    @Test
+    public void CanSaveANewPerson() throws Exception {
+        MockHttpServletResponse response = mvc.perform(
+                post("/api/persons").contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new PersonInfoDto(1L, "John Doe")))
+        ).andReturn().getResponse();
 
+        assertThat(response.getStatus()).isEqualTo(CREATED.value());
+    }
 }
