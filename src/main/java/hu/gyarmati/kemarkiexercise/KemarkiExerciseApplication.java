5f39b60e4778a8227e43dac313377e1e2d97499a
gyarmatiadam <gyari12@gmail.com>
2024-02-21 09:30:11 +0100

    Init repository

---
+++ src/main/java/hu/gyarmati/kemarkiexercise/KemarkiExerciseApplication.java
@@ -0,0 +1,13 @@
+package hu.gyarmati.kemarkiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KemarkiExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KemarkiExerciseApplication.class, args);
+    }
+
+}
