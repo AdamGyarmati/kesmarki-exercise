f654bd5e1a32aa38a5961497e27e5e8d44a0243b
gyarmatiadam <gyari12@gmail.com>
2024-02-21 10:59:25 +0100

    Write canSavePerson test in PersonServiceTest

--- src/test/java/hu/gyarmati/kemarkiexercise/service/PersonServiceTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/service/PersonServiceTest.java
@@ -5,6 +5,8 @@ import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.dto.SavePersonDto;
 import hu.gyarmati.kemarkiexercise.repository.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,6 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
@@ -46,5 +52,16 @@ public class PersonServiceTest {
                 .build();
     }
 
+    @DisplayName("Test for savePerson method")
+    @Test
+    public void canSavePerson() {
+        doReturn(person).when(modelMapper).map(savePersonDto, Person.class);
+        doReturn(personInfoDto).when(modelMapper).map(person, PersonInfoDto.class);
 
+        given(personRepository.save(person)).willReturn(person);
+
+        PersonInfoDto savedPerson = personServiceImp.savePerson(savePersonDto);
+
+        assertThat(savedPerson).isNotNull();
+    }
 }
