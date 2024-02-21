f7f48264cca526b05a8e1370cc832eb604b65f86
gyarmatiadam <gyari12@gmail.com>
2024-02-21 12:19:40 +0100

    Add logger to AddressController

--- src/main/java/hu/gyarmati/kemarkiexercise/controller/AddressController.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/controller/AddressController.java
@@ -1,12 +1,14 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
 import hu.gyarmati.kemarkiexercise.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/addresses")
+@Slf4j
 public class AddressController {
     private final AddressService addressService;
 
