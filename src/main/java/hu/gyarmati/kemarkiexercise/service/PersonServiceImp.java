f52ae4f46836e8df4e5b4c9ac8e90485e54a7b05
gyarmatiadam <gyari12@gmail.com>
2024-02-21 10:02:45 +0100

    Add PersonService interface and PersonServiceImp class

---
+++ src/main/java/hu/gyarmati/kemarkiexercise/service/PersonServiceImp.java
@@ -0,0 +1,9 @@
+package hu.gyarmati.kemarkiexercise.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PersonServiceImp implements PersonService {
+}
