8d498ac97dcf0b2e6463be98151908497abfa3cb
gyarmatiadam <gyari12@gmail.com>
2024-02-21 10:33:38 +0100

    Add dependencies to PersonServiceImp

--- src/main/java/hu/gyarmati/kemarkiexercise/service/PersonServiceImp.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/service/PersonServiceImp.java
@@ -2,12 +2,24 @@ package hu.gyarmati.kemarkiexercise.service;
 
 import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.dto.SavePersonDto;
+import hu.gyarmati.kemarkiexercise.repository.PersonRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class PersonServiceImp implements PersonService {
+    private final PersonRepository personRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PersonServiceImp(PersonRepository personRepository, ModelMapper modelMapper) {
+        this.personRepository = personRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public PersonInfoDto savePerson(SavePersonDto savePersonDto) {
         return null;
