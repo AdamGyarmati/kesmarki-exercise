d5a8685ce1c2ca5b6152dc5e352add1ea1c3953b
gyarmatiadam <gyari12@gmail.com>
2024-02-21 12:29:21 +0100

    Write canSaveANewAddress test method in AddressControllerTest

--- src/test/java/hu/gyarmati/kemarkiexercise/controller/AddressControllerTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/controller/AddressControllerTest.java
@@ -1,11 +1,21 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.gyarmati.kemarkiexercise.dto.AddressInfoDto;
+import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
 import hu.gyarmati.kemarkiexercise.service.AddressService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(AddressController.class)
 public class AddressControllerTest {
@@ -17,4 +27,15 @@ public class AddressControllerTest {
 
     @MockBean
     private AddressService addressService;
+
+    @DisplayName("Test for saveAddress method")
+    @Test
+    public void canSaveANewAddress() throws Exception {
+        MockHttpServletResponse response = mvc.perform(
+                post("/api/addresses").contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new AddressInfoDto(1L, "PERMANENT")))
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(CREATED.value());
+    }
 }
