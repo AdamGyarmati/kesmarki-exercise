674400f812573c7c23f5cc754b1fe9c94586cc56
gyarmatiadam <gyari12@gmail.com>
2024-02-21 12:24:20 +0100

    Start write saveAddress method in AddressController and add AddressInfoDto and SaveAndUpdateAddressDto

--- src/main/java/hu/gyarmati/kemarkiexercise/controller/AddressController.java
+++ src/main/java/hu/gyarmati/kemarkiexercise/controller/AddressController.java
@@ -1,10 +1,19 @@
 package hu.gyarmati.kemarkiexercise.controller;
 
+import hu.gyarmati.kemarkiexercise.dto.AddressInfoDto;
+import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
+import hu.gyarmati.kemarkiexercise.dto.SaveAndUpdateAddressDto;
+import hu.gyarmati.kemarkiexercise.dto.SaveAndUpdatePersonDto;
 import hu.gyarmati.kemarkiexercise.service.AddressService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/addresses")
@@ -15,5 +24,10 @@ public class AddressController {
     @Autowired
     public AddressController(AddressService addressService) {
         this.addressService = addressService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressInfoDto> saveAddress(@RequestBody SaveAndUpdateAddressDto saveAndUpdateAddressDto) {
+        return null;
     }
 }
