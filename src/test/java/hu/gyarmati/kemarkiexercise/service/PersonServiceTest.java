e3936eb59a9ead46a6c1d419c57cfe90c427e396
gyarmatiadam <gyari12@gmail.com>
2024-02-21 10:56:25 +0100

    Write BeforeEach in PersonServiceTest

--- src/test/java/hu/gyarmati/kemarkiexercise/service/PersonServiceTest.java
+++ src/test/java/hu/gyarmati/kemarkiexercise/service/PersonServiceTest.java
@@ -1,11 +1,17 @@
 package hu.gyarmati.kemarkiexercise.service;
 
+import hu.gyarmati.kemarkiexercise.domain.Person;
+import hu.gyarmati.kemarkiexercise.dto.PersonInfoDto;
+import hu.gyarmati.kemarkiexercise.dto.SavePersonDto;
 import hu.gyarmati.kemarkiexercise.repository.PersonRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.util.Collections;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
@@ -17,4 +23,28 @@ public class PersonServiceTest {
 
     @InjectMocks
     private PersonServiceImp personServiceImp;
+
+    private Person person;
+    private SavePersonDto savePersonDto;
+    private PersonInfoDto personInfoDto;
+
+    @BeforeEach
+    public void setup() {
+        person = Person.builder()
+                .id(1L)
+                .name("John Doe")
+                .addressList(Collections.emptyList())
+                .build();
+
+        savePersonDto = SavePersonDto.builder()
+                .name("John Doe")
+                .build();
+
+        personInfoDto = PersonInfoDto.builder()
+                .id(1L)
+                .name("John Doe")
+                .build();
+    }
+
+
 }
