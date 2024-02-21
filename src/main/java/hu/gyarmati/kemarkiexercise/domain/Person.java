fae87e9af1ee0eef456764769ecb178bbdcb662d
gyarmatiadam <gyari12@gmail.com>
2024-02-21 09:50:42 +0100

    Add Person entity

---
+++ src/main/java/hu/gyarmati/kemarkiexercise/domain/Person.java
@@ -0,0 +1,25 @@
+package hu.gyarmati.kemarkiexercise.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "person")
+    private List<Address> addressList;
+}
